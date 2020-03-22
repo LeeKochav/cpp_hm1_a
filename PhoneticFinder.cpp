23f9fb31b85af39cf8a638dd08d18c431bd06164
LeeKochav <leekochav@gmail.com>
2020-03-22 20:23:40 +0200

    hpp,cpp 100-test,cpp

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,13 @@
+#include "PhoneticFinder.hpp"
+
+#include <iostream>
+#include <string> 
+#include <stdexcept>
+using namespace std;
+
+string phonetic::find(string text,string word)
+{
+    return "ab";
+}
+
+
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string> 
+
+namespace phonetic
+{
+    std::string find(std::string text,std::string word);
+};
